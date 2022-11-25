68f7daf15cdae1a604fcdc878bc1d17549385736
Carlos Jimenez <cajimenezca@unal.edu.co>
2022-11-24 23:04:40 -0500

    stats router created

--- be-social-api/app.js
+++ be-social-api/app.js
@@ -1,12 +1,13 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+let indexRouter = require("./routes/index");
+let statsRouter = require("./routes/index");
 
-var app = express();
+let app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/stats", statsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
